649cfc0626bca06f26273ce30bb6b947475365bc
Anuj Kumar <98866899+anujjsengar@users.noreply.github.com>
2022-11-10 19:25:50 +0530

    Create check triangle by angle.js

---
+++ check triangle by angle.js
@@ -0,0 +1,14 @@
+var a=prompt("enter a first angle:");
+var b=prompt("enter a second angle:");
+var c=prompt("enter a third angle");
+var a=parseInt(a);
+var b=parseInt(b);
+var c=parseInt(c);
+var x=a+b+c
+if (x==180){
+  prompt("it is a triangle")
+}
+else{
+  prompt("It is not triangle")
+  
+}
