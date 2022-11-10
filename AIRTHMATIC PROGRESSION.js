7ae150d412763920c7ebac4f89e09e55ead2ec83
Anuj Kumar <98866899+anujjsengar@users.noreply.github.com>
2022-11-10 20:40:36 +0530

    Create AIRTHMATIC PROGRESSION.js

---
+++ AIRTHMATIC PROGRESSION.js
@@ -0,0 +1,9 @@
+var a=parseInt(prompt("enter first term"));
+var d=parseInt(prompt("common difference"));
+var n=parseInt(prompt("no of term"));
+var b=1;
+while(b<(n+1)){
+  x=a+b*d-d
+  b++
+  alert(x)
+}
